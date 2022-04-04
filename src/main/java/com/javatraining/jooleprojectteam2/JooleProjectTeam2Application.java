cdab7c3f24c1f38615abc9d19ece2bba6bab2233
slee293 <slee293@student.gsu.edu>
2022-04-04 16:51:38 -0400

    Initial commit

---
+++ src/main/java/com/javatraining/jooleprojectteam2/JooleProjectTeam2Application.java
@@ -0,0 +1,13 @@
+package com.javatraining.jooleprojectteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooleProjectTeam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooleProjectTeam2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatraining/jooleprojectteam2/JooleProjectTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatraining.jooleprojectteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooleProjectTeam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
