c252583e6265de3c2b50502dc5826cf9d6fa9e52
slee293 <slee293@student.gsu.edu>
2022-04-06 17:00:07 -0400

    created constructors and getters and setters, toString

--- src/main/java/com/javatraining/jooleprojectteam2/Entity/ProductType.java
+++ src/main/java/com/javatraining/jooleprojectteam2/Entity/ProductType.java
@@ -19,11 +19,77 @@ public class ProductType {
     private String mountingLocation;
     private String accessories;
 
+    // constructor
+    public ProductType(Product product, String useType, String application, String mountingLocation, String accessories) {
+        this.product = product;
+        this.useType = useType;
+        this.application = application;
+        this.mountingLocation = mountingLocation;
+        this.accessories = accessories;
+    }
+
+    public ProductType() {
+
+    }
+
+    // getter and setters
     public int getProductTypeId() {
         return productTypeId;
     }
 
     public void setProductTypeId(int productTypeId) {
         this.productTypeId = productTypeId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getUseType() {
+        return useType;
+    }
+
+    public void setUseType(String useType) {
+        this.useType = useType;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getMountingLocation() {
+        return mountingLocation;
+    }
+
+    public void setMountingLocation(String mountingLocation) {
+        this.mountingLocation = mountingLocation;
+    }
+
+    public String getAccessories() {
+        return accessories;
+    }
+
+    public void setAccessories(String accessories) {
+        this.accessories = accessories;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductType{" +
+                "productTypeId=" + productTypeId +
+                ", product=" + product +
+                ", useType='" + useType + '\'' +
+                ", application='" + application + '\'' +
+                ", mountingLocation='" + mountingLocation + '\'' +
+                ", accessories='" + accessories + '\'' +
+                '}';
     }
 }
--- src/main/java/com/javatraining/jooleprojectteam2/Entity/TechnicalDetail.java
+++ src/main/java/com/javatraining/jooleprojectteam2/Entity/TechnicalDetail.java
@@ -20,11 +20,87 @@ public class TechnicalDetail {
     private int fanSweepDiameter;
     private int height;
 
+    // constructors
+    public TechnicalDetail(Product product, int airflow, int maxPower, int soundAtMaxSpeed, int fanSweepDiameter, int height) {
+        this.product = product;
+        this.airflow = airflow;
+        this.maxPower = maxPower;
+        this.soundAtMaxSpeed = soundAtMaxSpeed;
+        this.fanSweepDiameter = fanSweepDiameter;
+        this.height = height;
+    }
+
+    public TechnicalDetail() {
+
+    }
+
+    // getter and setters
     public int getTechnicalDetailId() {
         return technicalDetailId;
     }
 
     public void setTechnicalDetailId(int technicalDetailId) {
         this.technicalDetailId = technicalDetailId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getAirflow() {
+        return airflow;
+    }
+
+    public void setAirflow(int airflow) {
+        this.airflow = airflow;
+    }
+
+    public int getMaxPower() {
+        return maxPower;
+    }
+
+    public void setMaxPower(int maxPower) {
+        this.maxPower = maxPower;
+    }
+
+    public int getSoundAtMaxSpeed() {
+        return soundAtMaxSpeed;
+    }
+
+    public void setSoundAtMaxSpeed(int soundAtMaxSpeed) {
+        this.soundAtMaxSpeed = soundAtMaxSpeed;
+    }
+
+    public int getFanSweepDiameter() {
+        return fanSweepDiameter;
+    }
+
+    public void setFanSweepDiameter(int fanSweepDiameter) {
+        this.fanSweepDiameter = fanSweepDiameter;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "TechnicalDetail{" +
+                "technicalDetailId=" + technicalDetailId +
+                ", product=" + product +
+                ", airflow=" + airflow +
+                ", maxPower=" + maxPower +
+                ", soundAtMaxSpeed=" + soundAtMaxSpeed +
+                ", fanSweepDiameter=" + fanSweepDiameter +
+                ", height=" + height +
+                '}';
     }
 }
