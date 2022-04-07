fe9f8767c13752641adab3cd19b6b65a229bde01
lbasevich <lbasevich86@gmail.com>
2022-04-06 22:31:13 -0400

    basic ProductProjectService CRUD

--- src/main/java/com/javatraining/jooleprojectteam2/Service/ProjectProductService.java
+++ src/main/java/com/javatraining/jooleprojectteam2/Service/ProjectProductService.java
@@ -1,9 +1,15 @@
 package com.javatraining.jooleprojectteam2.Service;
 
+import com.javatraining.jooleprojectteam2.Entity.Product;
+import com.javatraining.jooleprojectteam2.Entity.ProjectProduct;
 import com.javatraining.jooleprojectteam2.Repository.ProjectProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 public interface ProjectProductService {
+    ProjectProduct create (ProjectProduct projectproduct);
+    ProjectProduct find (String projectProductId);
+    ProjectProduct update (String projectProductId, ProjectProduct projectproduct);
+    ProjectProduct delete (ProjectProduct projectproduct);
 
 }
