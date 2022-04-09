020eed62ddc3767f936322177623e5efa55261ab
lbasevich <lbasevich86@gmail.com>
2022-04-09 16:48:31 -0400

    Project Controller

---
+++ src/main/java/com/javatraining/jooleprojectteam2/Controller/ProjectController.java
@@ -0,0 +1,62 @@
+package com.javatraining.jooleprojectteam2.Controller;
+
+import com.javatraining.jooleprojectteam2.Entity.Project;
+import com.javatraining.jooleprojectteam2.Service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+
+        @Autowired
+        ProjectService projectService;
+
+        // PostmanURL: localhost:8080/product/create
+        @PostMapping("/create")
+        public ResponseEntity<?> createProject(@RequestBody Project project) {
+                try {
+                        projectService.create(project);
+                } catch(Exception e) {
+                        return new ResponseEntity<>("{\"error\":\""+e.getMessage() + "\"}", HttpStatus.BAD_REQUEST);
+                }
+                return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        // PostmanURL: localhost:8080/project/find?projectId=X
+        @GetMapping("/find")
+        public ResponseEntity<?> findProject(@RequestParam(name = "projectId") int projectId) {
+                Project res;
+                try {
+                        res = projectService.findOne(projectId);
+                } catch(Exception e) {
+                        return new ResponseEntity<>("{\"error\":\""+e.getMessage() + "\"}", HttpStatus.BAD_REQUEST);
+                }
+                return new ResponseEntity<>(res, HttpStatus.CREATED);
+
+        }
+        // PostmanURL: localhost:8080/project/updateProject?projectId=X
+        @PutMapping("/updateProject")
+        public ResponseEntity<?> updateProject(@RequestBody Project project, @RequestParam(name="projectId") int projectId) {
+                Project temp;
+                try {
+                        temp = projectService.findOne(projectId);
+                        temp.setUser(project.getUser());
+                        temp.setProductSet(project.getProductSet());
+                        temp.setLastUpdated(new Date());
+                        projectService.update(temp);
+                } catch(Exception e) {
+                        return new ResponseEntity<>("{\"error\":\""+e.getMessage() + "\"}", HttpStatus.BAD_REQUEST);
+                }
+                return new ResponseEntity<>(temp, HttpStatus.CREATED);
+        }
+        // PostmanURL: localhost:8080/project/deleteProject?projectId=X
+        @DeleteMapping("/deleteProduct")
+        public ResponseEntity<?> deleteProject(@RequestParam(name="projectId") int projectId) {
+                projectService.delete(projectId);
+                return new ResponseEntity<>(HttpStatus.OK);
+        }
+}
